188122c770c72755ee9e7277dd4a8d0d6e23c8a9
Vy Nguyen <vy1872@gmail.com>
2022-09-01 15:41:18 -0700

    updated the coments columns:

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,44 @@
 const { Sequelize, Model, Datatypes } = require(`sequelize`);
 const sequelize = require(`../config/config`);
 
-class Comment extends Model {}
+class Comment extends Model { }
 
 Comment.init(
-    {
-      body: {
-        type: DataTypes.STRING,
-        allowNull: false
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncremnet: true
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: `User`,
+        key: `id`
       }
     },
-    {
-      sequelize
+    post_id: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: `Post`,
+        key: `id`
+      }
     }
-  );
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: `Comment`
+  }
+);
 
 module.exports = Comment;
