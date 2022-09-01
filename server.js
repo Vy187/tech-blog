60c869cf998b9d36a8bdb0bb812e7249fe0c6856
Vy Nguyen <vy1872@gmail.com>
2022-09-01 02:30:19 -0700

    got the server connected to the database

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require(`express`);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => { console.log(`App listing at http://localhost:${PORT}`)})
+const sequelize = require(`./config/config`);
+
+app.listen(PORT, () => { 
+    console.log(`App listing at http://localhost:${PORT}`);
+    sequelize.sync({ force: false});
+    })
