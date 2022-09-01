14ad23e77d83a69fff0739df830f7a4ade7fa6be
Vy Nguyen <vy1872@gmail.com>
2022-09-01 12:55:40 -0700

    decided to add more columns for post

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,39 @@
 const { Sequelize, Model, DataTypes } = require(`sequelize`);
 const sequelize = require(`../config/config`);
 
-class Post extends Model {}
+class Post extends Model { }
 
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncremnet: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: `User`,
+        key: `id`
+      }
+    }
   },
   {
-    sequelize
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: `Post`
   }
 );
 
