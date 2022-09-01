2b94a4de558bb2a97610c33376525d3f647a255f
Vy Nguyen <vy1872@gmail.com>
2022-09-01 02:59:03 -0700

    made the columns and used bcrypt for the user's password for the user table

--- models/User.js
+++ models/User.js
@@ -1,3 +1,50 @@
-class User extends Model {}
+const { Model, Datatypes } = require(`sequelize`);
+const bcrypt = require(`bcrypt`);
+const sequelize = require(`../config/config`);
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncremnet: true
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: `User`
+    }
+)
 
 module.exports = User;
