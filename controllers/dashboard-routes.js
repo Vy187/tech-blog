c4671e11fb06cc945d3891516133f15a204c027c
Vy Nguyen <vy1872@gmail.com>
2022-09-01 22:30:35 -0700

    made a route that get a post id and renders the edit-post hb if logged in

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -17,4 +17,19 @@ router.get(`/new`, withAuth, (req, res) => {
     res.render(`new-post`, { layout: `dashboard` });
 })
 
+router.get(`/:id`, withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render(`edit-post`, { layout: `dashboard`, post});
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.redirect(`login`);
+    }
+})
+
 module.exports = router;
