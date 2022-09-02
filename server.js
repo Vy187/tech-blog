983feb9bdf4ac49434db9a4ceca9a638332218ab
Vy Nguyen <vy1872@gmail.com>
2022-09-01 18:42:29 -0700

    got the intial routes to run with the server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require(`express`);
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require(`./config/config`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
+app.use(require(`./controllers/`))
 
 app.listen(PORT, () => { 
     console.log(`App listing at http://localhost:${PORT}`);
