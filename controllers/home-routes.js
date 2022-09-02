74d70035d01c288248f77dfea97bfd4683a415f9
Vy Nguyen <vy1872@gmail.com>
2022-09-01 21:29:55 -0700

    made route to get a single post and to render it's data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,4 +11,18 @@ router.get(`/`, async (req, res) => {
     }
 })
 
+router.get(`/:id`, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, { include: [User, { model: Comment, include: [User] }] });
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render(`single-post`, { post });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
