44f0c299f7f7b6a16a2275e6118f5d1407f20186
Vy Nguyen <vy1872@gmail.com>
2022-09-01 23:51:52 -0700

    made a router where user can delete a post if logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -24,4 +24,17 @@ router.put(`/:id`, withAuth, async (req, res) => {
     }
 })
 
+router.delete(`/:id`, withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Post.destroy({ where: { id: req.params.id } });
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
