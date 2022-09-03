17cc256aaaa9f3615901c98a643c6b76a978c232
Vy Nguyen <vy1872@gmail.com>
2022-09-03 14:04:00 -0700

    found a typo

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,7 @@ const withAuth = require(`../utils/auth`);
 
 router.get(`/`, withAuth, async (req, res) => {
     try {
-        const postData = await Post.findAll({ where: {userId: req.seesion.userId }});
+        const postData = await Post.findAll({ where: {userId: req.session.userId }});
         const posts = postData.map((post) => post.get({ plain: true }));
 
         res.render(`all-posts-admin`, { layout: `dashboard`, posts });
