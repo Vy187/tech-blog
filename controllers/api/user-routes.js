c250c625251c61b543d1e70316d210e844986cff
Vy Nguyen <vy1872@gmail.com>
2022-09-02 18:22:47 -0700

    forgot add to the route that it is a post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -37,7 +37,7 @@ router.post(`/login`, async (req, res) => {
     }
 })
 
-router(`/logout`, (req, res) => {
+router.post(`/logout`, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => { res.status(204).end() });
     } else {
