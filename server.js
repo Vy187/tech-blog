182caf85a8b367f398e968efc24cf00a9ed16024
Vy Nguyen <vy1872@gmail.com>
2022-09-02 18:11:44 -0700

    made the app able to parse incoming data put it into req

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require(`./config/config`);
+const sequelize = require(`./config/connection`);
 const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.use(require(`./controllers/`))
 
 app.listen(PORT, () => { 
     console.log(`App listing at http://localhost:${PORT}`);
-    sequelize.sync({ force: false});
-    })
+    sequelize.sync({ force: false });
+})
