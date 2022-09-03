057a345778eaaf7ba19686d5747b762ece65be72
Vy Nguyen <vy1872@gmail.com>
2022-09-02 19:27:09 -0700

    made the server able to take in the handlebars

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require(`path`);
 const express = require(`express`);
 const session = require("express-session");
+const exphbs = require(`express-handlebars`);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,8 +9,14 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require(`./config/connection`);
 const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
 
+const hbs = exphbs.create();
+
+app.engine(`handlebars`, hbs.engine);
+app.set(`view engine`, `handlebars`)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, `public`)));
 
 app.use(require(`./controllers/`))
 
