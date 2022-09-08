92046fb5e16d5a4c26ba0d9f6546a6857a38b6ac
Vy Nguyen <vy1872@gmail.com>
2022-09-07 21:38:04 -0700

    made it where that if the user is logged in that they can make a comment while if they aren't then they can't be prompted to make a comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,7 +16,7 @@ router.get(`/post/:id`, async (req, res) => {
         const postData = await Post.findByPk(req.params.id, { include: [User, { model: Comment, include: [User] }] });
         if (postData) {
             const post = postData.get({ plain: true });
-            res.render(`single-post`, { post });
+            res.render(`single-post`, { post, loggedIn: req.session.loggedIn });
         } else {
             res.status(404).end();
         }
